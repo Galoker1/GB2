192edd7e1d65aa1155d07806a04832734d1cfb09
Egor Khlyamov <egor.hlyamov@yandex.ru>
2022-10-15 23:16:55 +0300

    Add lesson 1

---
+++ Lesson1/Lesson1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lesson1
+//
+//  Created by Егор  Хлямов on 09.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
